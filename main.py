3be5f745e5857da423974491dc6510e9a009667c
Pushkal Rajendra Burde <pushkalrajendraburde@MBK-TECH-767.local>
2025-06-23 15:46:21 +0530

    changed main.py file

--- main.py
+++ main.py
@@ -1,18 +1,62 @@
-from fastmcp import FastMCP
 
-mcp = FastMCP()
-
-@mcp.tool()
-def multiply(a: float, b: float) -> float:
-    """This tool returns multiplication of two numbers"""
-    return a * b
-
-@mcp.tool()
-def greet(name: str) -> str:
-    return f"Good morning {name}. Have a nice day"
 
 # This is only for local development
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
 
+
+import asyncio
+import logging
+import os
+
+from fastmcp import FastMCP 
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="[%(levelname)s]: %(message)s", level=logging.INFO)
+
+mcp = FastMCP("MCP Server on Cloud Run")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Use this to add two numbers together.
+    
+    Args:
+        a: The first number.
+        b: The second number.
+    
+    Returns:
+        The sum of the two numbers.
+    """
+    logger.info(f">>> Tool: 'add' called with numbers '{a}' and '{b}'")
+    return a + b
+
+@mcp.tool()
+def subtract(a: int, b: int) -> int:
+    """Use this to subtract two numbers.
+    
+    Args:
+        a: The first number.
+        b: The second number.
+    
+    Returns:
+        The difference of the two numbers.
+    """
+    logger.info(f">>> Tool: 'subtract' called with numbers '{a}' and '{b}'")
+    return a - b
+
+@mcp.tool()
+def greet(name: str) -> str:
+    return f"Good morning {name}. Have a nice day"
+
+
+if __name__ == "__main__":
+    logger.info(f" MCP server started on port {os.getenv('PORT', 8080)}")
+    # Could also use 'sse' transport, host="0.0.0.0" required for Cloud Run.
+    asyncio.run(
+        mcp.run_async(
+            transport="streamable-http", 
+            host="0.0.0.0", 
+            port=os.getenv("PORT", 8080),
+        )
+    )
 
